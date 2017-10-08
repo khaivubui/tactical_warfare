62ab9b1037d7e7e272ca6f6340c81aa75e040e35
Michael <mzhu105@gmail.com>
2017-10-08 14:54:13 -0700

    Test from michael

--- frontend/tactical_warfare.js
+++ frontend/tactical_warfare.js
@@ -1,10 +1,10 @@
 console.log("hi");
 
-console.log('Hi this is Khai');
+console.log("Hi this is Khai");
 document.addEventListener("DOMContentLoaded", () => {
-
-  document.querySelector('.game').addEventListener(
-    'click',
-    () => alert('AHAHAHAHAHAH')
-  );
+  document
+    .querySelector(".game")
+    .addEventListener("click", () => alert("AHAHAHAHAHAH"));
 });
+
+// DO YOU SEE THIS KHAI???
