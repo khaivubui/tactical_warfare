f7d3195c3c1d5c93731f16695330f6bce310edc2
Khai Bui <khaivubui@gmail.com>
2017-10-08 12:30:52 -0700

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './frontend/tactical_warfare.js',
+  output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.js$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '*']
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ]
+};
