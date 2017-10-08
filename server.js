b39e526e33fc7b0271a2faef89b2462826e5707f
Khai Bui <khaivubui@gmail.com>
2017-10-08 12:10:40 -0700

    Set up basic view

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ import express from  'express';
 
 const app = express();
 
+// ---------- Root Route ----------
+
+app.get(
+  '/',
+  (req, res) => {
+    res.render('root.html');
+  }
+);
+
+// ---------- Start Server ----------
+
 const port = process.env.PORT || 3000;
+
 app.listen(
   port,
   () => console.log(`Server started on port ${port}`)
