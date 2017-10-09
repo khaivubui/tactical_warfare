518481ff0f155f863767505952854eadb37263b4
axadn <adanmarrufo@gmail.com>
2017-10-08 18:09:51 -0700

    add tank model

--- frontend/tactical_warfare.js
+++ frontend/tactical_warfare.js
@@ -10,8 +10,8 @@ const createScene = function createScene(){
    camera.setTarget(BABYLON.Vector3.Zero());
    camera.attachControl(canvas, false);
    const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
-   const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
-   const ground = BABYLON.Mesh.CreateGround('ground1', 100,100,2, scene);
+   //const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
+   //const ground = BABYLON.Mesh.CreateGround('ground1', 100,100,2, scene);
 
    window.addEventListener('resize', ()=> {
      engine.resize();
@@ -19,9 +19,21 @@ const createScene = function createScene(){
    engine.runRenderLoop(()=>{
      scene.render();
    });
+
+   BABYLON.SceneLoader.ImportMesh("Cube.001", "models/tanks/sand_tank/",
+    "sand_tank.babylon", scene,
+   function (newMeshes) {
+
+        // Set the target of the camera to the first imported mesh
+        camera.target = newMeshes[0];
+    });
+    debugger;
+   return scene;
 }
 
 const startGame = function startGame(){
   const scene = createScene();
+
+  //const tank_mesh = new sand_tank.Cube_001("tank1",scene, "");
 };
 document.addEventListener("DOMContentLoaded", startGame);
