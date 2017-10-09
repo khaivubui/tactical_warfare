78fc3d1a25d6e0cfa66670678373cb924444bc46
axadn <adanmarrufo@gmail.com>
2017-10-08 22:12:31 -0700

    initial setup of babylon.js

--- frontend/tactical_warfare.js
+++ frontend/tactical_warfare.js
@@ -4,13 +4,11 @@ const createScene = function createScene(){
   const engine = new BABYLON.Engine(canvas, true);
   const scene = new BABYLON.Scene(engine);
 
-  const camera = new BABYLON.FreeCamera('camera',
-   new BABYLON.Vector3(0,5,-10), scene);
-
-   camera.setTarget(BABYLON.Vector3.Zero());
+  const camera = new BABYLON.ArcRotateCamera('camera', 0,0.8, 5,
+   new BABYLON.Vector3.Zero(), scene);
    camera.attachControl(canvas, false);
    const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
-   //const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
+   //const sphere = BABYLON.Mesh.    window.tank_mesh = tank_mesh;CreateSphere('sphere1', 16, 2, scene);
    //const ground = BABYLON.Mesh.CreateGround('ground1', 100,100,2, scene);
 
    window.addEventListener('resize', ()=> {
@@ -20,14 +18,13 @@ const createScene = function createScene(){
      scene.render();
    });
 
-   BABYLON.SceneLoader.ImportMesh("Cube.001", "models/tanks/sand_tank/",
+   const tank_mesh = BABYLON.SceneLoader.ImportMesh("Cube.001", "models/tanks/sand_tank/",
     "sand_tank.babylon", scene,
    function (newMeshes) {
-
         // Set the target of the camera to the first imported mesh
         camera.target = newMeshes[0];
     });
-    debugger;
+
    return scene;
 }
 
