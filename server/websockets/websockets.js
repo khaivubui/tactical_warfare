c2d80db3f67e733f01925315b95caca6fb0b91df
Khai Bui <khaivubui@gmail.com>
2017-10-10 13:20:50 -0700

    Restructure socket.io on the backend and add fake usernames

--- server/websockets/websockets.js
+++ server/websockets/websockets.js
@@ -1,10 +1,18 @@
+const faker = require('faker');
+
 module.exports = io => {
-  const activeSockets = [];
+  const activeSockets = {};
 
   io.on('connection', socket => {
-    console.log(`${socket.id} has connected`);
-    activeSockets.push(socket.id);
-
+    // console.log(`${socket.id} has connected`);
+    activeSockets[socket.id] = {
+      opponentSocketId: null,
+      displayName:
+      `${faker.name.firstName()} the ${faker.commerce.productName()}`
+    };
+    console.log(
+      `${activeSockets[socket.id].displayName} has connected`
+    );
 
     socket.on('disconnect', reason => {
       console.log(`${socket.id} disconnected because ${reason}`);
