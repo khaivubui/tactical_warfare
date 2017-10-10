3a82f37ec68b97186362bd317b12a8e7765b4540
Michael <mzhu105@gmail.com>
2017-10-09 21:13:04 -0700

    Add client folder which contains session_form and react components

---
+++ client/session_form/session_form_container.js
@@ -0,0 +1,10 @@
+import { connect } from "react-redux";
+import SessionForm from "./session_form";
+
+
+const mapStateToProps = state => {
+  return {
+    loggedIn: Boolean(state.session.currentUser),
+    
+  }
+}
