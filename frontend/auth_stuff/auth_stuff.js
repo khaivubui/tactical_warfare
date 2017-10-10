81b81b6b904dd0c7b615d19d6a751e199bda64d3
Michael <mzhu105@gmail.com>
2017-10-10 10:12:35 -0700

    Add login form transition

--- frontend/auth_stuff/auth_stuff.js
+++ frontend/auth_stuff/auth_stuff.js
@@ -1,4 +1,15 @@
 export default () => {
   const registerForm = document.querySelector("#register-form");
-  const signInForm = document.querySelector("#signin-form");
+  const signinForm = document.querySelector("#signin-form");
+  const registerButton = document.querySelector('#register-button');
+  const signinButton = document.querySelector('#signin-button');
+
+  registerButton.addEventListener('click', () => {
+    registerForm.style['max-height'] = '200px';
+    signinForm.style['max-height'] = '0px';
+  });
+  signinButton.addEventListener('click', () => {
+    registerForm.style['max-height'] = '0px';
+    signinForm.style['max-height'] = '200px';
+  });
 };
