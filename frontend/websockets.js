731fd7ef572151b620d1a3c3bbec731330105810
Khai Bui <khaivubui@gmail.com>
2017-10-10 18:36:45 -0700

    Get the slide out from the right working

--- frontend/websockets.js
+++ frontend/websockets.js
@@ -2,4 +2,19 @@ export default socket => {
   socket.on('currentSocket', data => {
     window.currentSocket = data;
   });
+
+
+  const activeSocketsToggle =
+  document.querySelector('.active-sockets-toggle');
+  const activeSocketsWidget =
+  document.querySelector('.active-sockets-widget');
+  activeSocketsWidget.style.right = "-100px";
+
+  activeSocketsToggle.addEventListener('click', () => {
+    if (activeSocketsWidget.style.right === "-100px") {
+      activeSocketsWidget.style.right = '50%';
+    } else {
+      activeSocketsWidget.style.right = "-100px";
+    }
+  });
 };
