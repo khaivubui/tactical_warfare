e7e9b13a4e4fb3c182f4211d452d1d3761e1ab67
Khai Bui <khaivubui@gmail.com>
2017-10-12 09:16:13 -0700

    Cleanup code again

--- server/routes/users.js
+++ server/routes/users.js
@@ -64,10 +64,15 @@ router.post('/authenticate', (req, res, next) => {
   });
 });
 
-// Profile (protected route because of passport.authenticate('jwt', {session:false}))
-router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
-  res.json({user: req.user});
-});
+// Profile
+// (protected route because of passport.authenticate('jwt', {session:false}))
+router.get(
+  '/profile',
+  passport.authenticate('jwt', {session:false}),
+  (req, res, next) => {
+    res.json({user: req.user});
+  }
+);
 
 
 module.exports = router;
