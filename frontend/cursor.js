2244ef4e5993af22c443385930fbbba1539ffa02
Khai Bui <khaivubui@gmail.com>
2017-10-13 11:35:26 -0700

    Change color for cursor

--- frontend/cursor.js
+++ frontend/cursor.js
@@ -6,7 +6,9 @@ export default class Cursor{
     this._displayPosition = new BABYLON.Vector3.Zero();
     this._mesh = new BABYLON.Mesh.CreatePlane("cursor", size, scene);
     this._mesh.material = new BABYLON.StandardMaterial("mat1", scene);
-    this._mesh.material.diffuseColor = new BABYLON.Color3(0.5, 0, 0);
+    this._mesh.material.diffuseColor = new BABYLON.Color3(
+      0.15, 0.68, 0.376
+    );
     this._mesh.material.alpha = 0.9;
     this._mesh.rotation.x = Math.PI/2
   }
