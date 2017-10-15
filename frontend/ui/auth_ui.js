cc18a16cfafa94ad1fb60152db35b9f34859902b
Khai Bui <khaivubui@gmail.com>
2017-10-15 09:47:56 -0700

    Visually sign in a user

--- frontend/ui/auth_ui.js
+++ frontend/ui/auth_ui.js
@@ -1,3 +1,18 @@
+import { closeAuthWidget } from '../auth_stuff/auth_stuff';
+
 export const signInAs = username => {
-  
+  closeAuthWidget();
+  const currentUser = document.createElement('div');
+  currentUser.classList.add('current-user');
+  currentUser.innerHTML = `Hello, ${username}`;
+
+  const authStatus = document.querySelector('.auth-status');
+  authStatus.style.width = '90%';
+  authStatus.prepend(currentUser);
+
+  const authWidgetToggle = document.querySelector('.auth-widget-toggle');
+  authWidgetToggle.innerHTML = 'Sign Out';
+
+  const currentSocket = document.querySelector('.current-socket');
+  currentSocket.innerHTML = username;
 };
