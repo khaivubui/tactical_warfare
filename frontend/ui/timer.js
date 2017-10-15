4ee48e8284f1232b8ed81c7eaecfda110ab704b3
Khai Bui <khaivubui@gmail.com>
2017-10-14 17:19:36 -0700

    Fix timer bug

--- frontend/ui/timer.js
+++ frontend/ui/timer.js
@@ -4,14 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
   timerWidget = document.querySelector('.timer-widget');
 });
 
+let timerTimeout;
+
 export const renderTimer = timerDuration => {
   timerWidget.innerHTML = Math.floor(timerDuration / 1000);
   if (timerDuration <= 0) {
     timerWidget.innerHTML = '';
     return;
   }
-  window.setTimeout(
+  timerTimeout = window.setTimeout(
     () => renderTimer(timerDuration - 1000),
     1000
   );
+};
+
+export const clearTimer = () => {
+  timerWidget.innerHTML = '';
+  window.clearTimeout(timerTimeout);
 };
