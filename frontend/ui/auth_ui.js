36295194b4b6827615f7d7fc4473a4b3f07aa580
Khai Bui <khaivubui@gmail.com>
2017-10-15 14:19:17 -0700

    Finish auth

--- frontend/ui/auth_ui.js
+++ frontend/ui/auth_ui.js
@@ -1,4 +1,7 @@
+import Cookie from 'js-cookie';
+
 import { closeAuthWidget } from '../auth_stuff/auth_stuff';
+import { socket } from '../websockets';
 
 export const signInAs = username => {
   closeAuthWidget();
@@ -12,4 +15,21 @@ export const signInAs = username => {
 
   const authWidgetToggle = document.querySelector('.auth-widget-toggle');
   authWidgetToggle.innerHTML = 'Sign Out';
+  authWidgetToggle.addEventListener('click', signOut);
 };
+
+export const signOut = () => {
+  const currentUser = document.querySelector('.current-user');
+  const authStatus = document.querySelector('.auth-status');
+  authStatus.removeChild(currentUser);
+  authStatus.style.width = 'auto';
+
+  const authWidgetToggle = document.querySelector('.auth-widget-toggle');
+  authWidgetToggle.innerHTML = 'Play now';
+  authWidgetToggle.removeEventListener('click', signOut);
+
+  Cookie.set('auth-token', '');
+  socket.emit('signOut');
+};
+
+window.signOut = signOut;
