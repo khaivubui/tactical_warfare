90dd8ac0e58ae252b3a8e546ccdeed3602c0d4ae
Michael <mzhu105@gmail.com>
2017-10-15 19:24:28 -0700

    prettier frontend files

--- frontend/ui/camera_rotation.js
+++ frontend/ui/camera_rotation.js
@@ -1,13 +1,13 @@
 let cameraRotationStyle;
 
-document.addEventListener('DOMContentLoaded', () => {
-  cameraRotationStyle = document.querySelector('.camera-rotation').style;
+document.addEventListener("DOMContentLoaded", () => {
+  cameraRotationStyle = document.querySelector(".camera-rotation").style;
 });
 
 export const hideCameraRotation = () => {
-  cameraRotationStyle['z-index'] = '-100';
+  cameraRotationStyle["z-index"] = "-100";
 };
 
 export const showCameraRotation = () => {
-  cameraRotationStyle['z-index'] = '100';
+  cameraRotationStyle["z-index"] = "100";
 };
