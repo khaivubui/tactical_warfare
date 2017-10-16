0fd7a79173b76df23a485f7821405a38552c1d63
Michael <mzhu105@gmail.com>
2017-10-15 19:22:24 -0700

    prettier player file

--- frontend/player.js
+++ frontend/player.js
@@ -1,19 +1,22 @@
-import {storeCameraState, restoreCameraState} from './game_utils/camera_utils';
-import {socket} from "./websockets";
-import {hideCameraRotation, showCameraRotation} from './ui/camera_rotation';
+import {
+  storeCameraState,
+  restoreCameraState
+} from "./game_utils/camera_utils";
+import { socket } from "./websockets";
+import { hideCameraRotation, showCameraRotation } from "./ui/camera_rotation";
 
 const AIMING_CAMERA_ROT_SPEED = 0.05;
 const AIMING_CAMERA_RADIUS = 2;
 const AIMING_CAMERA_HEIGHT = 1;
 
 const AIMING_MAX_X_ROT = 0.9;
-const AIMING_MIN_X_ROT = - 0.5;
+const AIMING_MIN_X_ROT = -0.5;
 
 const TANK_OPTIONS_WIDTH = 200;
 const TANK_CANNON_LENGTH = 1.8;
 
-export class Player{
-  constructor(tank){
+export class Player {
+  constructor(tank) {
     this.tank = tank;
     this.health = 100;
 
@@ -21,70 +24,66 @@ export class Player{
     this._rotXMesh = null;
     this._rotYMesh = null;
     let name;
-    for(let i = 0; i < childMeshes.length; ++i){
+    for (let i = 0; i < childMeshes.length; ++i) {
       name = childMeshes[i].name;
       name = name.split(".");
-      name = name[name.length-1];
-      if(name === "tank_rot_x"){
+      name = name[name.length - 1];
+      if (name === "tank_rot_x") {
         this._rotXMesh = childMeshes[i];
-      }
-      else if (name === "tank_rot_y"){
+      } else if (name === "tank_rot_y") {
         this._rotYMesh = childMeshes[i];
       }
     }
   }
-  startListeningForAttack(onDoneCallback){
+  startListeningForAttack(onDoneCallback) {
     onDoneCallback(new BABYLON.Matrix.Identity());
   }
-  receiveDamage(amount){
+  receiveDamage(amount) {
     this.health -= amount;
   }
 
-  resetCannon(){
+  resetCannon() {
     this._rotXMesh.rotation.x = 0;
     this._rotYMesh.rotation.y = this.tank.rotation.y;
   }
 }
 
-
-export class DemoPlayer extends Player{
-  constructor(tank){
+export class DemoPlayer extends Player {
+  constructor(tank) {
     super(tank);
   }
 
-  startListeningForMoveOptions(onDoneCallback){
+  startListeningForMoveOptions(onDoneCallback) {
     onDoneCallback("position");
   }
 
-  startListeningForPosition(onDoneCallback){
+  startListeningForPosition(onDoneCallback) {
     onDoneCallback(this.tank.position);
   }
-  endTurn() {
-
-  }
+  endTurn() {}
 }
 
-export class SocketPlayer extends Player{
-  constructor(tank){
+export class SocketPlayer extends Player {
+  constructor(tank) {
     super(tank);
     this._rotateOpponentPos = this._rotateOpponentPos.bind(this);
     this._rotateOpponentAttack = this._rotateOpponentAttack.bind(this);
     this.stopListeningForPosition = this.stopListeningForPosition.bind(this);
-    this.stopListeningForMoveOptions = this.stopListeningForMoveOptions.bind(this);
+    this.stopListeningForMoveOptions = this.stopListeningForMoveOptions.bind(
+      this
+    );
     this.stopListeningForAttack = this.stopListeningForAttack.bind(this);
-
   }
 
-  startListeningForPosition(onDoneCallback, onCancelledCallback){
-    socket.on("position", pos =>{
+  startListeningForPosition(onDoneCallback, onCancelledCallback) {
+    socket.on("position", pos => {
       onDoneCallback(this._rotateOpponentPos(pos));
       this.stopListeningForPosition();
     });
     socket.on("cancel", () => {
       onCancelledCallback();
       this.stopListeningForPosition();
-    }
-  );
+    });
   }
   // Turn off socket when LocalPlayer is emitting positions
   stopListeningForPosition() {
@@ -92,46 +91,44 @@ export class SocketPlayer extends Player{
     socket.off("cancel");
   }
   // Socket player listens for opponent's move options
-  startListeningForMoveOptions(onDoneCallback){
-    socket.on("moveType", type=>{
+  startListeningForMoveOptions(onDoneCallback) {
+    socket.on("moveType", type => {
       onDoneCallback(type);
       this.stopListeningForMoveOptions();
     });
   }
 
   // Turn off socket when LocalPlayer is emitting initial move choices
-  stopListeningForMoveOptions(){
+  stopListeningForMoveOptions() {
     socket.off("moveType");
   }
 
-  startListeningForAttack(onDoneCallback, onCancelledCallback){
-    socket.on("attack", matrix=> {
+  startListeningForAttack(onDoneCallback, onCancelledCallback) {
+    socket.on("attack", matrix => {
       onDoneCallback(this._rotateOpponentAttack(matrix));
       this.stopListeningForAttack();
-    }
-    );
+    });
     socket.on("cancel", () => {
       onCancelledCallback();
       this.stopListeningForAttack();
     });
   }
 
-  stopListeningForAttack(){
+  stopListeningForAttack() {
     socket.off("attack");
     socket.off("cancel");
   }
 
-
-  _rotateOpponentPos(pos){
-    return BABYLON.Vector3.TransformCoordinates(pos,
-      BABYLON.Matrix.RotationAxis(BABYLON.Axis.Y, Math.PI));
+  _rotateOpponentPos(pos) {
+    return BABYLON.Vector3.TransformCoordinates(
+      pos,
+      BABYLON.Matrix.RotationAxis(BABYLON.Axis.Y, Math.PI)
+    );
   }
-  _rotateOpponentAttack(matrix){
+  _rotateOpponentAttack(matrix) {
     const mat = new BABYLON.Matrix.Identity();
     mat.m = matrix.m;
-    return mat.multiply(
-      BABYLON.Matrix.RotationAxis(BABYLON.Axis.Y,Math.PI)
-    );
+    return mat.multiply(BABYLON.Matrix.RotationAxis(BABYLON.Axis.Y, Math.PI));
   }
 
   endTurn() {
@@ -141,8 +138,8 @@ export class SocketPlayer extends Player{
   }
 }
 
-export class LocalPlayer extends Player{
-  constructor(tank, scene, arena){
+export class LocalPlayer extends Player {
+  constructor(tank, scene, arena) {
     super(tank);
     this.arena = arena;
     this.scene = scene;
@@ -156,30 +153,30 @@ export class LocalPlayer extends Player{
     this._handleZoomOut = this._handleZoomOut.bind(this);
   }
 
-  _handleMoveOption(onDoneCallback){
+  _handleMoveOption(onDoneCallback) {
     return type => e => {
       this._stopListeningForMoveOptions();
-      socket.emit('moveType', type);
+      socket.emit("moveType", type);
       onDoneCallback(type);
     };
   }
 
-  _stopListeningForMoveOptions(){
-    const turnOptions = document.getElementById('turn-options');
-    const attack = document.getElementById('attack-button');
-    const move = document.getElementById('move-button');
-    const forfeit = document.getElementById('forfeit-button');
-    turnOptions.style['max-width'] = '0';
+  _stopListeningForMoveOptions() {
+    const turnOptions = document.getElementById("turn-options");
+    const attack = document.getElementById("attack-button");
+    const move = document.getElementById("move-button");
+    const forfeit = document.getElementById("forfeit-button");
+    turnOptions.style["max-width"] = "0";
     attack.onclick = null;
     move.onClick = null;
     forfeit.onClick = null;
   }
 
-  startListeningForMoveOptions(onDoneCallback){
-    this._maximizeTankOptions('turn-options');
-    const attack = document.getElementById('attack-button');
-    const move = document.getElementById('move-button');
-    const forfeit = document.getElementById('forfeit-button');
+  startListeningForMoveOptions(onDoneCallback) {
+    this._maximizeTankOptions("turn-options");
+    const attack = document.getElementById("attack-button");
+    const move = document.getElementById("move-button");
+    const forfeit = document.getElementById("forfeit-button");
     const zoomin = document.querySelector(".zoom-in");
     const zoomout = document.querySelector(".zoom-out");
     attack.onclick = this._handleMoveOption(onDoneCallback)("attack");
@@ -189,102 +186,106 @@ export class LocalPlayer extends Player{
     zoomout.onclick = this._handleZoomOut();
   }
 
-  _handleConfirmPosition(onDoneCallback){
-    return position =>{
+  _handleConfirmPosition(onDoneCallback) {
+    return position => {
       this._stopListeningForPosition();
       socket.emit("position", position);
       onDoneCallback(position);
     };
   }
 
-  _stopListeningForPosition(){
-    this._minimizeTankOptions('position-options');
+  _stopListeningForPosition() {
+    this._minimizeTankOptions("position-options");
     this.arena.ground.cancelListeningForPosition();
   }
 
-  startListeningForPosition(onDoneCallback, onCancelledCallback){
-    this._maximizeTankOptions('position-options');
+  startListeningForPosition(onDoneCallback, onCancelledCallback) {
+    this._maximizeTankOptions("position-options");
     const cancel = document.querySelector("#position-options .cancel-button");
-    cancel.onclick =()=>{
+    cancel.onclick = () => {
       this._stopListeningForPosition();
       this.arena.ground.cancelListeningForPosition();
       socket.emit("cancel");
       onCancelledCallback();
     };
-    this.arena.ground.startListeningForPosition(this._handleConfirmPosition(
-      onDoneCallback));
+    this.arena.ground.startListeningForPosition(
+      this._handleConfirmPosition(onDoneCallback)
+    );
   }
-  _positionAimingCamera(){
+  _positionAimingCamera() {
     const camera = this.scene.activeCamera;
     const cameraTarget = this._rotXMesh.getAbsolutePosition().clone();
     cameraTarget.y += AIMING_CAMERA_HEIGHT;
     camera.target = cameraTarget;
     camera.radius = AIMING_CAMERA_RADIUS;
-    camera.alpha = -1* this._rotYMesh.rotation.y - Math.PI/2;
-    camera.beta = this._rotXMesh.rotation.x + Math.PI/2;
+    camera.alpha = -1 * this._rotYMesh.rotation.y - Math.PI / 2;
+    camera.beta = this._rotXMesh.rotation.x + Math.PI / 2;
   }
-  _maximizeTankOptions(id){
+  _maximizeTankOptions(id) {
     const options = document.getElementById(id);
-    options.style['max-width'] = `${TANK_OPTIONS_WIDTH}px`;
+    options.style["max-width"] = `${TANK_OPTIONS_WIDTH}px`;
   }
-  _minimizeTankOptions(id){
+  _minimizeTankOptions(id) {
     const options = document.getElementById(id);
-    options.style['max-width'] = '0';
+    options.style["max-width"] = "0";
   }
-  startListeningForAttack(onDoneCallback, onCancelledCallback){
+  startListeningForAttack(onDoneCallback, onCancelledCallback) {
     showCameraRotation();
-    this._maximizeTankOptions('attack-options');
+    this._maximizeTankOptions("attack-options");
     const camera = this.scene.activeCamera;
     const canvas = document.getElementById("render-canvas");
     const rotationWidget = document.querySelector(".camera-rotation");
     const fire = document.getElementById("fire-button");
     const cancel = document.querySelector("#attack-options .cancel-button");
-    cancel.onclick = ()=>{
+    cancel.onclick = () => {
       this._stopListeningForAttack();
-       socket.emit("cancel");
-       onCancelledCallback();
-     };
-    fire.onclick = () =>{
+      socket.emit("cancel");
+      onCancelledCallback();
+    };
+    fire.onclick = () => {
       this._stopListeningForAttack();
       const projectileMatrix = this._calculateProjectileMatrix();
       socket.emit("attack", projectileMatrix);
       onDoneCallback(projectileMatrix);
     };
     this.originalRotationWidgetMouseDown = rotationWidget.onmousedown;
-    rotationWidget.onmousedown  = this.handleAimingMouseDown;
+    rotationWidget.onmousedown = this.handleAimingMouseDown;
     this._storeCameraState();
     this._setUpAimingCamera();
     this._positionAimingCamera();
-
   }
-  _calculateProjectileMatrix(){
-      const tankCannonMatrix = this._rotXMesh.worldMatrixFromCache;
-      const bombOffsetLocal = new BABYLON.Matrix.Translation(0,0, -TANK_CANNON_LENGTH);
-      return bombOffsetLocal.multiply(tankCannonMatrix);
+  _calculateProjectileMatrix() {
+    const tankCannonMatrix = this._rotXMesh.worldMatrixFromCache;
+    const bombOffsetLocal = new BABYLON.Matrix.Translation(
+      0,
+      0,
+      -TANK_CANNON_LENGTH
+    );
+    return bombOffsetLocal.multiply(tankCannonMatrix);
   }
-  _stopListeningForAttack(){
+  _stopListeningForAttack() {
     hideCameraRotation();
     this._restoreCameraState();
-    this._minimizeTankOptions('attack-options');
+    this._minimizeTankOptions("attack-options");
     const rotationWidget = document.querySelector(".camera-rotation");
-      rotationWidget.onmousedown   = this.originalRotationWidgetMouseDown;
+    rotationWidget.onmousedown = this.originalRotationWidgetMouseDown;
   }
 
-  _setUpAimingCamera(){
+  _setUpAimingCamera() {
     const camera = this.scene.activeCamera;
     camera.lowerAlphaLimit = null;
     camera.upperAlphaLimit = null;
     camera.lowerBetaLimit = null;
     camera.upperBetaLimit = null;
-    camera.lowerRadiusLimit =0;
+    camera.lowerRadiusLimit = 0;
     camera.radius = AIMING_CAMERA_RADIUS;
     camera.detachControl(this.previousCameraState.inputs.attachedElement);
   }
-  _storeCameraState(){
+  _storeCameraState() {
     const camera = this.scene.activeCamera;
     this.previousCameraState = storeCameraState(camera);
   }
-  _restoreCameraState(){
+  _restoreCameraState() {
     const camera = this.scene.activeCamera;
     if (this.previousCameraState) {
       restoreCameraState(camera, this.previousCameraState);
@@ -292,33 +293,33 @@ export class LocalPlayer extends Player{
     }
     // const canvas = document.getElementById("render-canvas");
   }
-  handleAimingMouseDrag(e){
+  handleAimingMouseDrag(e) {
     const deltaX = e.screenX - this.previousMouseX;
     const deltaY = e.screenY - this.previousMouseY;
     this._rotYMesh.rotation.y += deltaX * AIMING_CAMERA_ROT_SPEED;
     this._rotXMesh.rotation.x -= deltaY * AIMING_CAMERA_ROT_SPEED;
     this.previousMouseX = e.screenX;
     this.previousMouseY = e.screenY;
-    if(this._rotXMesh.rotation.x > AIMING_MAX_X_ROT){
+    if (this._rotXMesh.rotation.x > AIMING_MAX_X_ROT) {
       this._rotXMesh.rotation.x = AIMING_MAX_X_ROT;
     }
-    if(this._rotXMesh.rotation.x < AIMING_MIN_X_ROT){
+    if (this._rotXMesh.rotation.x < AIMING_MIN_X_ROT) {
       this._rotXMesh.rotation.x = AIMING_MIN_X_ROT;
     }
     this._positionAimingCamera();
   }
-  handleAimingMouseDown(e){
+  handleAimingMouseDown(e) {
     this.previousMouseX = e.screenX;
     this.previousMouseY = e.screenY;
     window.onmousemove = this.handleAimingMouseDrag;
     window.onmouseup = this.handleAimingMouseUp;
   }
-  handleAimingMouseUp(e){
+  handleAimingMouseUp(e) {
     window.onmousemove = null;
     window.onmouseup = null;
   }
   // Zoom in button on the left
-  _handleZoomIn(){
+  _handleZoomIn() {
     return e => {
       if (this.scene.activeCamera.radius > 0) {
         this.scene.activeCamera.radius -= 3;
@@ -326,14 +327,14 @@ export class LocalPlayer extends Player{
     };
   }
   // Zoom out button on the left
-  _handleZoomOut(){
+  _handleZoomOut() {
     return e => {
       this.scene.activeCamera.radius += 3;
     };
   }
 
   // For end turn, turn off all socket player sockets
-  endTurn(){
+  endTurn() {
     this._stopListeningForMoveOptions();
     this._stopListeningForAttack();
     this._stopListeningForPosition();
@@ -345,13 +346,13 @@ export class LocalPlayer extends Player{
     health.innerHTML = this.health;
   }
   // hide "forfeit" button in Demo game
-  hideForfeitButton(){
-    const forfeitWrapper = document.getElementById('forfeit-wrapper');
+  hideForfeitButton() {
+    const forfeitWrapper = document.getElementById("forfeit-wrapper");
     forfeitWrapper.style["max-height"] = "0px";
   }
   // show "forfeit" button in Online game
   showForfeitButton() {
-    const forfeitWrapper = document.getElementById('forfeit-wrapper');
+    const forfeitWrapper = document.getElementById("forfeit-wrapper");
     forfeitWrapper.style["max-height"] = "140px";
   }
 }
